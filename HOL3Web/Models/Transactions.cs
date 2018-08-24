29b6e53cf16db6b9e50e77939d222f65005ac239
Aditya Gholba <aditya.gholba@gmail.com>
2018-08-24 18:39:42 +0530

    Test push. Added Transactions.cs

---
+++ HOL3Web/Models/Transactions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HOL3Web.Models
+{
+    public class Transactions
+    {
+        int transactionID;
+        int bookID;
+        int userID;
+
+    }
+}
